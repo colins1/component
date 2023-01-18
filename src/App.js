07f9b3adc10cca34c451af242422d7bc257f9dfb
colins1 <maxrozhkov1@gmail.com>
2023-01-18 21:04:24 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import UserProfile from './UserProfile';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const item = {
+	  brand: 'Tiger of Sweden',
+	  title: 'Leonard coat',
+	  description: 'Minimalistic coat in cotton-blend',
+	  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+	  price: 399,
+	  currency: '£'
+	}
+	return <UserProfile item={item} />;
 }
 
 export default App;
