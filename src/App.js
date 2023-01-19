bf2f89dad7faa66e50a42316daeebaf203c73b28
colins1 <maxrozhkov1@gmail.com>
2023-01-19 10:01:25 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import UserProfile from './UserProfile';
 
+
 function App() {
 	const item = {
-	  brand: 'Tiger of Sweden',
-	  title: 'Leonard coat',
-	  description: 'Minimalistic coat in cotton-blend',
-	  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-	  price: 399,
-	  currency: '£'
+		brand: 'Tiger of Sweden',
+		title: 'Leonard coat',
+		description: 'Minimalistic coat in cotton-blend',
+		descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+		price: 399,
+		currency: '£'
 	}
-	return <UserProfile item={item} />;
+
+	return (  <div className="container">
+    <div className="background-element">
+    </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+      <ShopItemFunc item={item} />
+    </div>
+  </div>);
 }
 
 export default App;
--- src/UserProfile.js
+++
@@ -1,6 +0,0 @@
-function UserProfile(item){
-	return (
-        <div>{item.map(items => <li>{items}</li>)}</div>
-        );
-}
-export default UserProfile;
